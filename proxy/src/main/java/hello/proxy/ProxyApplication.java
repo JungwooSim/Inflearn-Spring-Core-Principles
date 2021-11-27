b4cc2d71ea75acabebebf8f7a46995ba76465fc7
JungwooSim <cscd053@gmail.com>
2021-11-27 11:30:47 +0900

    프록시 패턴과 데코레이터 패턴 / 예제 프로젝트 만들기 v2

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,12 @@
 package hello.proxy;
 
 import hello.config.AppV1Config;
+import hello.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+@Import({AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
